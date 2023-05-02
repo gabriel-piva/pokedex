62983b22f66a0b5a85f9a1954e795e19c746808c
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2023-05-02 13:42:29 -0300

    modals & filters (first versions)

--- js/main.js
+++ js/main.js
@@ -105,6 +105,15 @@ const loadPokedex = async () => {
 
 // -------------------------------------------------------------------------- 
 
+// Type Filter
+
+const typeFilter = (e) => {
+    console.log(e.target.dataset.type);
+    closeModal();
+}
+
+// -------------------------------------------------------------------------- 
+
 //  Mobile Menu
 
 let btnMobileChanged = false;
@@ -146,6 +155,91 @@ const scrollTop = () => {
 
 // -------------------------------------------------------------------------- 
 
+// Modals
+
+const modal = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modalContainer');
+const modalContent = document.querySelector('.modalContent');
+
+const openModal = () => {
+    modal.classList.add('active');
+    modalContainer.classList.add('active');
+}
+const closeModal = () => {
+    modal.classList.remove('active');
+    modalContainer.classList.remove('active');
+    modalContainer.classList.remove('modalType');
+    modalContent.innerHTML = '';
+}
+
+// Modal Type Filter
+const modalType = () => {
+    openModal();
+    modalContent.innerHTML = `
+        <span>Select a type:</span>
+        <div class="types">
+            <button type="button" class="btnType">
+                <img src="images/types/water.png" data-type="water">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/steel.png" data-type="steel">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/rock.png" data-type="rock">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/psychic.png" data-type="psychic">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/poison.png" data-type="poison">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/normal.png" data-type="normal">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/ice.png" data-type="ice">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/ground.png" data-type="ground">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/grass.png" data-type="grass">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/ghost.png" data-type="ghost">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/flying.png" data-type="flying">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/fire.png" data-type="fire">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/fighting.png" data-type="fighting">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/fairy.png" data-type="fairy">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/electric.png" data-type="electric">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/dragon.png" data-type="dragon">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/dark.png" data-type="dark">
+            </button>
+            <button type="button" class="btnType">
+                <img src="images/types/bug.png" data-type="bug">
+            </button>
+        </div>
+    `;
+    modalContainer.classList.add('modalType');
+    document.querySelectorAll(".btnType").forEach(btn => btn.addEventListener("click", (e) => typeFilter(e)));
+}
+
+// -------------------------------------------------------------------------- 
+
 // Events
 
 window.onload = loadPokedex;
@@ -153,5 +247,12 @@ regionButtons.forEach(btn => btn.addEventListener('click', (e) => changeRegion(e
 window.onscroll = verifyTop;
 scrollBtn.addEventListener('click', scrollTop);
 mobileBtn.addEventListener('click', toggleMobile);
+document.querySelector('#filterBtn').addEventListener('click', modalType);
+document.querySelector("#closeModalBtn").addEventListener('click', closeModal);
+document.querySelector('.modal').addEventListener('click', (e) => {
+    if(e.target == document.querySelector('.modal')) {
+        closeModal() 
+    }
+});
 
 // -------------------------------------------------------------------------- 
